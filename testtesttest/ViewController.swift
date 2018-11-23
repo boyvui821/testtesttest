0c2ab8200393ec7f25f5da5fc5f00ed543cda3f4
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-11-23 11:39:09 +0700

    Initial Commit

---
+++ testtesttest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testtesttest
+//
+//  Created by Nguyen Hieu Trung on 11/23/18.
+//  Copyright © 2018 NHT-SOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
